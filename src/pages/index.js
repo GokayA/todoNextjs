e7799a736fa9cecb29be3c5b774127bae25cfd71
yokyaa <arjenrobbennn@gmail.com>
2023-04-16 17:11:43 +0300

    "cleaned index.js"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,4 @@
-import PrivateRoute from '@/components/PrivateRoute';
-import { Inter } from 'next/font/google';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const router = useRouter();
